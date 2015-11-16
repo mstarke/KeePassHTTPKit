915f84d0187414e41362153e09b4229a86106702
michael starke <michael.starke@hicknhack-software.com>
2015-11-16 08:59:07 +0100

    Creating Framework, adding submodules, added Carthage file
    Signed-off-by: michael starke <michael.starke@hicknhack-software.com>

---
+++ KeePassHTTPKit/KeePassHTTPKit.h
@@ -0,0 +1,19 @@
+//
+//  KeePassHTTPKit.h
+//  KeePassHTTPKit
+//
+//  Created by Michael Starke on 16/11/15.
+//  Copyright © 2015 HicknHack Software GmbH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KeePassHTTPKit.
+FOUNDATION_EXPORT double KeePassHTTPKitVersionNumber;
+
+//! Project version string for KeePassHTTPKit.
+FOUNDATION_EXPORT const unsigned char KeePassHTTPKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeePassHTTPKit/PublicHeader.h>
+
+
