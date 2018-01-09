f25c83626a0aa696680dac73b0493efa8ae079f4
Michael Starke <michael.starke@hicknhack-software.com>
2018-01-09 13:48:49 +0100

    codestyle

--- KeePassHTTPKit/Handlers/KPHGetAllLoginsHandler.m
+++ KeePassHTTPKit/Handlers/KPHGetAllLoginsHandler.m
@@ -18,26 +18,26 @@
   }
   
   NSString *key = [self delegateKeyForLabel:request.Id];
-  if (!key)
+  if (!key) {
     return;
+  }
   
   KPHAESConfig *aes = [KPHAESConfig aesWithKey:key base64key:YES IV:request.Nonce base64IV:YES];
   NSURL *url = [NSURL URLWithString:[KPHUtils decryptString:request.Url withAES:aes]];
-  if (!url)
+  if (!url) {
     return;
+  }
   
   NSArray *entries = [self delegateAllEntries];
   
   response.Id = request.Id;
   [self setResponseVerifier:response];
   
-  if (entries)
-  {
+  if (entries) {
     entries = [self copyEntries:entries];
     
     /* these should not be provided in get-all-logins */
-    for (KPHResponseEntry __strong *entry in entries)
-    {
+    for (KPHResponseEntry *entry in entries) {
       entry.Password = nil;
       entry.StringFields = nil;
     }
